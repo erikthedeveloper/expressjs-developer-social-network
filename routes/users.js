c47be8de6697cfeeb52bfeebc783cc9900b4223a
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-12 09:44:39 -0700

    1st Revision "/users/*" RESTful routes and views (.jade)
    - Adds views/users/*.jade, updates routes... adds form for new...
    - Adds links between users RESTful views.....

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
+var users = {
+  '1': {
+    id: 1,
+    name: 'Joseph Myers'
+  },
+  '2': {
+    id: 2,
+    name: 'Sally Smith'
+  },
+  '3': {
+    id: 3,
+    name: 'John Doe'
+  }
+};
+
 /**
  * Capture and Set Up :person_id from route parameters
  */
 router.param('person_id', function(req, res, next, person_id) {
-    var people = {
-        '1': {name: 'Joseph Myers'},
-        '2': {name: 'Sally Smith'},
-        '3': {name: 'John Doe'}
-    };
-    req.person = people[person_id] || "No User Found!";
+    req.user = users[person_id] || "No User Found!";
     next();
 });
 
 /* users.index */
 router.get('/', function(req, res) {
-  res.render('index', {title: 'Users Index'});
+  res.render('users/index', {users: users});
 });
 
 /* users.new */
 router.get('/new', function(req, res) {
-  res.render('index', {title: 'Users New'});
+  res.render('users/new', {user: req.user});
 });
 
 /* users.create */
 router.post('/', function(req, res) {
-  res.render('index', {title: 'Users Create'});
+  var response_data = {
+    name: req.param('name')
+  };
+  res.send(response_data);
 });
 
 /* users.show */
 router.get('/:person_id', function(req, res) {
-  res.render('index', {title: 'Users Show: ' + req.person.name});
+  res.render('users/show', {user: req.user});
 });
 
 /* users.edit */
 router.get('/:person_id/edit', function(req, res) {
-  res.render('index', {title: 'Users Edit: ' + req.person.name});
+  res.render('users/edit', {user: req.user});
 });
 
 /* users.update */
 router.put('/:person_id', function(req, res) {
-  res.render('index', {title: 'Users Update: ' + req.person.name});
+    res.send("Updating user! (" + req.user.name + ")");
 });
 
 /* users.destroy */
 router.delete('/:person_id', function(req, res) {
-  res.render('index', {title: 'Users Destroy: ' + req.person.name});
+    res.send("Destroy! (" + req.user.name + ")");
 });
 
 module.exports = router;
